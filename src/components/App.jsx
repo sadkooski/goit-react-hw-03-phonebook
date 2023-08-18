72d8765ca3e8386f570e8730354494974573b629
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-08-18 12:39:07 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,14 @@ import { Filter } from 'components/Filter/Filter';
 class App extends Component {
   constructor() {
     super();
-    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+
     this.state = {
-      contacts: [],
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
       filter: '',
       name: '',
       number: '',
